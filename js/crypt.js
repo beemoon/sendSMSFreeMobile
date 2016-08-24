292b3533f5a4888a7ac46b54b89c666ca7bc7957
Beemoon and Co <contact@beemoon.fr>
2016-08-24 14:32:33 +0200

    Add files via upload

---
+++ js/crypt.js
@@ -0,0 +1,28 @@
+$(function () {
+
+    $('#send-form').validator();
+
+    $('#send-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "crypt.php";
+            
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageText = data.message;
+
+                    if (messageText) {
+                        alert(messageText);
+                        $('#send-form')[0].reset();
+                    }
+                }
+            });
+            
+            
+            return false;
+        }
+    })
+});
